489204edf8b71255c47b0a0c489535ea1850164b
jkort <jortealv@myuax.com>
2023-03-25 17:32:48 +0100

    Ejercicio1.a)

--- src/main/java/Ejercicio1/Algortimo1.java
+++ src/main/java/Ejercicio1/Algortimo1.java
@@ -1,2 +1,11 @@
-package Ejercicio1;public class Algortimo1 {
+package Ejercicio1;
+
+/*a) La función bisect (0,n) calcula la raíz cuadrada de 2 mediante el método de bisección
+Es decir, encuentra un valor x tal que f(x) = x^2 - N = 0, con un margen de precisión de 1e-6.
+Si cambiamos el valor de N, estaríamos calculando la raíz cuadrada de ese nuevo valor.
+Si cambiamos la función f(x), estaríamos calculando la raíz de la ecuación definida por la nueva función.
+
+ */
+
+public class Algortimo1 {
 }
