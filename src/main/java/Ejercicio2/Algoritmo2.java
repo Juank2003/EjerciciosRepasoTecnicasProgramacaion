f6c30cad9245708631a6952d9a8af309caed5583
jkort <jortealv@myuax.com>
2023-03-25 17:51:10 +0100

    Ejercicio2.a)

---
+++ src/main/java/Ejercicio2/Algoritmo2.java
@@ -0,0 +1,8 @@
+package Ejercicio2;
+
+/* La llamada a la función f(x,2) muestra todos los factores primos del número x.
+El nombre más adecuado para la función f podría ser "imprimirFactoresPrimos".
+ */
+
+public class Algoritmo2 {
+}
